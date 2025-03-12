09530dbcfe1fded67b6e7e77b788d30897693fd7
Ivan Mikhaylov <ivmikh@taltech.ee>
2025-03-12 19:17:02 +0200

    123a

---
+++ meeskonnatoo/CTemperatuuriTeisendaja.vb
@@ -0,0 +1,11 @@
+﻿Public Class CTemperatuuriTeisendaja
+    Implements ITemperatuuriTeisendaja
+
+    Private Function ITemperatuuriTeisendaja_CelciusToFahrenheit(c As Double) As Double Implements ITemperatuuriTeisendaja.CelciusToFahrenheit
+        Return (c * 9 / 5) + 32
+    End Function
+
+    Private Function ITemperatuuriTeisendaja_FahrenheitToCelsius(f As Double) As Double Implements ITemperatuuriTeisendaja.FahrenheitToCelsius
+        Return (f - 32) * 5 / 9
+    End Function
+End Class
